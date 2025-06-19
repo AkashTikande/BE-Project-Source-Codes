24e5ca88cf254b1574ac6eb3094c65b25d9ee209
AkashTikande <akashtikande10117@gmail.com>
2025-06-19 12:58:48 +0530

    BE Project Source Code Updated

--- src/AdasSystemSource.java
+++ src/AdasSystemSource.java
@@ -6,111 +6,107 @@ public class AdasSystemSource {
             //Arduino IDE
 
 /*
-            System.out.println("This is C++ code of project Advanced " +
-                    "ADAS system using Raspberry pi");
-            System.out.println("                   #include <ESP8266WebServer-impl.h>\n" +
-                    "#include <ESP8266WebServer.h>\n" +
-                    "#include <ESP8266WebServerSecure.h>\n" +
-                    "#include <Parsing-impl.h>\n" +
-                    "#include <Uri.h>\n" +
-                    "\n" +
-                    "#include <SoftwareSerial.h> // serial coomunication with oithe digital component\n" +
-                    "#include <TinyGPS++.h>\n" +
-                    "\n" +
-                    "SoftwareSerial gsmSerial(D2, D3);  // SIM800L (TX-D2, RX-D3)\n" +
-                    "SoftwareSerial gpsSerial(D5, D6);  // NEO-7M GPS (TX-D5, RX-D6)\n" +
-                    "\n" +
-                    "TinyGPSPlus gps;\n" +
-                    "String senderNumber = \"+919359272856\";  // Receiver's number\n" +
-                    "\n" +
-                    "void setup() {\n" +
-                    "    Serial.begin(115200);\n" +
-                    "    gsmSerial.begin(9600);  // GSM baud rate\n" +
-                    "    gpsSerial.begin(9600);  // GPS baud rate (change to 4800 if needed)\n" +
-                    "    \n" +
-                    "    delay(1000);\n" +
-                    "    Serial.println(\"Initializing...\");\n" +
-                    "    delay(15000);\n" +
-                    "    initGSM();\n" +
-                    "}\n" +
-                    "\n" +
-                    "void loop() {\n" +
-                    "    checkSMS();\n" +
-                    "}\n" +
-                    "\n" +
-                    "// Function to initialize the GSM module\n" +
-                    "void initGSM() {\n" +
-                    "    gsmSerial.println(\"AT\"); // Check if GSM is responding\n" +
-                    "    delay(1000);\n" +
-                    "    gsmSerial.println(\"AT+CMGF=1\"); // Set SMS mode to text\n" +
-                    "    delay(1000);\n" +
-                    "    gsmSerial.println(\"AT+CNMI=2,2,0,0,0\"); // Enable SMS notification\n" +
-                    "    delay(1000);\n" +
-                    "}\n" +
-                    "\n" +
-                    "// Function to check for incoming SMS\n" +
-                    "void checkSMS() {\n" +
-                    "    if (gsmSerial.available()) {\n" +
-                    "        String msg = gsmSerial.readString();\n" +
-                    "        Serial.println(\"Received SMS: \" + msg);\n" +
-                    "        \n" +
-                    "        if (msg.indexOf(\"location\") != -1) {\n" +
-                    "            Serial.println(\"Location request received.\");\n" +
-                    "            se  ndLocation();\n" +
-                    "        }\n" +
-                    "    }\n" +
-                    "}\n" +
-                    "\n" +
-                    "// Function to get GPS location and send SMS\n" +
-                    "void sendLocation() {\n" +
-                    "    float latitude = 0.0, longitude = 0.0;\n" +
-                    "    Serial.println(\"Fetching GPS data...\");\n" +
-                    "\n" +
-                    "    unsigned long startTime = millis();\n" +
-                    "    bool gpsUpdated = false;\n" +
-                    "\n" +
-                    "    // Wait up to 30 seconds for valid GPS data\n" +
-                    "    while (millis() - startTime < 30000) {\n" +
-                    "        while (gpsSerial.available()) {\n" +
-                    "            char c = gpsSerial.read();\n" +
-                    "            Serial.write(c);  // Print raw GPS data for debugging\n" +
-                    "            gps.encode(c);\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        if (gps.location.isUpdated()) {\n" +
-                    "            latitude = gps.location.lat();\n" +
-                    "            longitude = gps.location.lng();\n" +
-                    "            gpsUpdated = true;\n" +
-                    "            break;\n" +
-                    "        }\n" +
-                    "    }\n" +
-                    "\n" +
-                    "    if (!gpsUpdated) {\n" +
-                    "        Serial.println(\"No GPS data available.\");\n" +
-                    "        gsmSerial.println(\"AT+CMGS=\\\"\" + senderNumber + \"\\\"\");\n" +
-                    "        delay(1000);\n" +
-                    "        gsmSerial.print(\"GPS signal not available. Try again.\");\n" +
-                    "        gsmSerial.write(26); // Send Ctrl+Z to end the message\n" +
-                    "        delay(3000);\n" +
-                    "    } else {\n" +
-                    "        String gmapLink = \"https://www.google.com/maps?q=\" + String(latitude, 6) + \",\" + String(longitude, 6);\n" +
-                    "        Serial.println(\"Sending location: \" + gmapLink);\n" +
-                    "        \n" +
-                    "        gsmSerial.println(\"AT+CMGS=\\\"\" + senderNumber + \"\\\"\");\n" +
-                    "        delay(1000);\n" +
-                    "        gsmSerial.print(\"Live Location: \" + gmapLink);\n" +
-                    "        gsmSerial.write(26); // Send Ctrl+Z to end the message\n" +
-                    "        delay(3000);\n" +
-                    "    }\n" +
-                    "}                     ");
 
 
-            System.out.println("This os a Python Code which holds algorithem to run the car");
+          #include <ESP8266WebServer-impl.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WebServerSecure.h>
+#include <Parsing-impl.h>
+#include <Uri.h>
+
+#include <SoftwareSerial.h> // serial coomunication with oithe digital component
+#include <TinyGPS++.h>
+
+SoftwareSerial gsmSerial(D2, D3);  // SIM800L (TX-D2, RX-D3)
+SoftwareSerial gpsSerial(D5, D6);  // NEO-7M GPS (TX-D5, RX-D6)
+
+TinyGPSPlus gps;
+String senderNumber = "+919359272856";  // Receiver's number
+
+void setup() {
+    Serial.begin(115200);
+    gsmSerial.begin(9600);  // GSM baud rate
+    gpsSerial.begin(9600);  // GPS baud rate (change to 4800 if needed)
+
+    delay(1000);
+    Serial.println("Initializing...");
+    delay(15000);
+    initGSM();
+}
+
+void loop() {
+    checkSMS();
+}
+
+// Function to initialize the GSM module
+void initGSM() {
+    gsmSerial.println("AT"); // Check if GSM is responding
+    delay(1000);
+    gsmSerial.println("AT+CMGF=1"); // Set SMS mode to text
+    delay(1000);
+    gsmSerial.println("AT+CNMI=2,2,0,0,0"); // Enable SMS notification
+    delay(1000);
+}
+
+// Function to check for incoming SMS
+void checkSMS() {
+    if (gsmSerial.available()) {
+        String msg = gsmSerial.readString();
+        Serial.println("Received SMS: " + msg);
+
+        if (msg.indexOf("location") != -1) {
+            Serial.println("Location request received.");
+            se  ndLocation();
+        }
+    }
+}
+
+// Function to get GPS location and send SMS
+void sendLocation() {
+    float latitude = 0.0, longitude = 0.0;
+    Serial.println("Fetching GPS data...");
+
+    unsigned long startTime = millis();
+    bool gpsUpdated = false;
+
+    // Wait up to 30 seconds for valid GPS data
+    while (millis() - startTime < 30000) {
+        while (gpsSerial.available()) {
+            char c = gpsSerial.read();
+            Serial.write(c);  // Print raw GPS data for debugging
+            gps.encode(c);
+        }
+
+        if (gps.location.isUpdated()) {
+            latitude = gps.location.lat();
+            longitude = gps.location.lng();
+            gpsUpdated = true;
+            break;
+        }
+    }
+
+    if (!gpsUpdated) {
+        Serial.println("No GPS data available.");
+        gsmSerial.println("AT+CMGS=\"" + senderNumber + "\"");
+        delay(1000);
+        gsmSerial.print("GPS signal not available. Try again.");
+        gsmSerial.write(26); // Send Ctrl+Z to end the message
+        delay(3000);
+    } else {
+        String gmapLink = "https://www.google.com/maps?q=" + String(latitude, 6) + "," + String(longitude, 6);
+        Serial.println("Sending location: " + gmapLink);
+
+        gsmSerial.println("AT+CMGS=\"" + senderNumber + "\"");
+        delay(1000);
+        gsmSerial.print("Live Location: " + gmapLink);
+        gsmSerial.write(26); // Send Ctrl+Z to end the message
+        delay(3000);
+    }
+}
+
 
 */
-            
 
-            // Thony Editor
 
 
         }
